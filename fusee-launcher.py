c4805714684279469df6d56dbfab12e8d455ba7a
Drims75 <drimalowsij@meta.ua>
2024-10-17 23:24:21 +0300

    Update fusee-launcher.py

--- fusee-launcher.py
+++ fusee-launcher.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 #
-# fusée gelée 1
+# fusée gelée 
 #
 # Launcher for the {re}switched coldboot/bootrom hacks--
 # launches payloads above the Horizon
@@ -46,15 +46,15 @@ RCM_V4P_HEADER_SIZE = 680
 
 # The address where the RCM payload is placed.
 # This is fixed for most device.
-RCM_PAYLOAD_ADDR    = 0x4000A000
+RCM_PAYLOAD_ADDR    = 0x40010000
 
 # The address where the user payload is expected to begin.
-PAYLOAD_START_ADDR  = 0x4000AE40
+PAYLOAD_START_ADDR  = 0x40010E40
 
 # Specify the range of addresses where we should inject our
 # payload address.
-STACK_SPRAY_START   = 0x4000EE40
-STACK_SPRAY_END     = 0x40011000
+STACK_SPRAY_START   = 0x40010E40
+STACK_SPRAY_END     = 0x40017000
 
 class RCMError(Exception):
     def __init__(self, rcm_error_code):
@@ -467,8 +467,8 @@ class RCMHax:
     DEFAULT_PID = 0x7330
 
     # Exploit specifics
-    COPY_BUFFER_ADDRESSES   = [0x40003000, 0x40005000]   # The addresses of the DMA buffers we can trigger a copy _from_.
-    STACK_END               = 0x4000A000                 # The address just after the end of the device's stack.
+    COPY_BUFFER_ADDRESSES   = [0x40005000, 0x40009000]   # The addresses of the DMA buffers we can trigger a copy _from_.
+    STACK_END               = 0x40010000                 # The address just after the end of the device's stack.
 
     def __init__(self, wait_for_device=False, os_override=None, vid=None, pid=None, override_checks=False):
         """ Set up our RCM hack connection."""
